b154cb214ee0f34692274fe67ab13e9c9e4dd5ed
emilecaron <emile.caron@outlook.com>
2018-05-18 09:34:11 +0200

    lint

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ class UploadCommand(Command):
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
+
 setup(
     # This is the name of your project. The first time you publish this
     # package, this name will be registered for you. It will determine how
